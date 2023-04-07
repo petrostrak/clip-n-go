e7803501bbdc0ace1aa67f31c880ab65f3d2507f
petrostrak <pit.trak@gmail.com>
2023-04-07 15:29:34 +0300

    Add fileserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,14 @@ func main() {
 	mux.HandleFunc("/clip", showClip)
 	mux.HandleFunc("/clip/create", createClip)
 
+	// Create a fileserver which serves files out of the "./ui/static" dir.
+	fs := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip
+	// the "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+
 	log.Println("Starting server on: 8080")
 	// Use the http.ListenAndServe() to start a new web server. We pass in
 	// the TCP network address to listen on and the servemux.
