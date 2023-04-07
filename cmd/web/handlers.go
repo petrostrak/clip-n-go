52513b792f92d8f13c0de265d2e259ab3401acbb
petrostrak <pit.trak@gmail.com>
2023-04-07 11:33:13 +0300

    refactor project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -34,20 +33,4 @@ func createClip(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Create a new clip...")) // Write will automatically send 200 OK if no WriteHeader() called
-}
-
-func main() {
-	// Use the http.NewServeMux() to initialize a new servemux, then
-	// register the home() as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/clip", showClip)
-	mux.HandleFunc("/clip/create", createClip)
-
-	log.Println("Starting server on: 8080")
-	// Use the http.ListenAndServe() to start a new web server. We pass in
-	// the TCP network address to listen on and the servemux.
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatal(err)
-	}
 }
