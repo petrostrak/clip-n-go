cdc6770dc065d48bfcf38ab5c33f7fb4a95d21d8
petrostrak <pit.trak@gmail.com>
2023-04-07 10:47:43 +0300

    customizing headers

--- main.go
+++ main.go
@@ -8,7 +8,27 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Clip 'n Go!" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Clip 'n Go!"))
+}
+
+func showClip(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific clip..."))
+}
+
+func createClip(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		w.WriteHeader(405) // It is possible to call WriteHeader() once per response
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
+	w.Write([]byte("Create a new clip...")) // Write will automatically send 200 OK if no WriteHeader() called
 }
 
 func main() {
@@ -16,6 +36,8 @@ func main() {
 	// register the home() as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/clip", showClip)
+	mux.HandleFunc("/clip/create", createClip)
 
 	log.Println("Starting server on: 8080")
 	// Use the http.ListenAndServe() to start a new web server. We pass in
