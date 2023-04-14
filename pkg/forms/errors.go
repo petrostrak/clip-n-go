3259e8ad0a358d94309340333e8a3991a6103db3
petrostrak <pit.trak@gmail.com>
2023-04-14 10:56:54 +0300

    Add errors package

---
+++ pkg/forms/errors.go
@@ -0,0 +1,19 @@
+package forms
+
+// errors holds the validation error messages for forms. The name of the
+// form field will be used as the key in this map.
+type errors map[string][]string
+
+// Add adds error messages for a given field to the map.
+func (e errors) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
+
+// Get retrieves the first error message for a given field from the map.
+func (e errors) Get(field string) string {
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+	return es[0]
+}
